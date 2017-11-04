e3b9cf4a96221adffc073bbae1b9ca4f8de6800a
Punkbooster <ugliar.arsen@gmail.com>
2017-11-04 18:58:41 +0100

    Make lunch blocks nicier

--- src/components/AddLunch.js
+++ src/components/AddLunch.js
@@ -1,16 +1,24 @@
 import React from 'react'
 
 const AddLunch = (props) => (
-  <div className='container'>
-    <h3>
-      Add Lunch
-    </h3>
-    <form className='bs-example bs-example-form col-md-8'>
-      <div className="input-group"> <span className="input-group-addon" id="basic-addon1">Place & Time</span>
-        <input className="form-control" aria-describedby="basic-addon1"/>
+  <div className='container col-md-12'>
+    <div className='col-md-6 pull-right'>
+      <div className='panel panel-primary pull-right'>
+        <div className='panel-heading'>
+          Add Lunch
+        </div>
+        <div className='panel-body'>
+          <form className='bs-example bs-example-form'>
+            <div className="input-group">
+              <span className="input-group-addon" id="basic-addon1">Place & Time</span>
+              <input className="form-control" aria-describedby="basic-addon1"/>
+            </div>
+            <br />
+            <button type="submit" className="btn btn-default">Submit</button>
+          </form>
+        </div>
       </div>
-      <button type="submit" className="btn btn-default">Submit</button>
-    </form>
+    </div>
   </div>
 )
 
--- src/components/Lunch.js
+++ src/components/Lunch.js
@@ -1,21 +1,27 @@
 import React from 'react'
 
 const Lunch = (props) => (
-  <div className="panel panel-primary col-md-6">
-     <div className="panel-heading">
-       {props.lunch.restaurantName} - {props.lunch.time}
-     </div>
-     <ul className="list-group">
-       {props.lunch.users.map(user => (
-         <li key={user} className="list-group-item">
-           {user}
-         </li>
-       ))}
-     </ul>
-     <button className='btn btn-danger' onClick={() => props.onRemoveClick(props.lunch.id)}>
-       Remove lunch
-     </button>
-   </div>
+  <div className='container col-md-6'>
+    <div className='panel panel-default'>
+      <div className='panel-heading'>
+        {props.lunch.restaurantName}
+        - {props.lunch.time}
+      </div>
+      <div className='panel-body'>
+        <ul className='list-group'>
+          {props.lunch.users.map(user => (
+            <li key={user} className='list-group-item'>
+              {user}
+            </li>
+          ))}
+        </ul>
+        <button className='btn btn-danger pull-right' onClick={() => props.onRemoveClick(props.lunch.id)}>
+          Remove lunch
+        </button>
+      </div>
+
+    </div>
+  </div>
 )
 
 export default Lunch
